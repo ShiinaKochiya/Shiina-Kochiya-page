c92f8e1c0a22c98b0da1ab45150f41011216deb1
Kochiya Shiina <caovanviethung1412@gmail.com>
2026-06-05 14:29:51 +0700

    omagad slider

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -16,81 +16,91 @@ import facebook from "#/socials/facebook.png"
 
 export default function Hero({}){
      return (
-    <div className="relative items-center justify-center font-sans bg-white h-auto">
-      <div className="absolute bg-linear-to-r from-transparent via-black to-black w-7/8 h-full right-0 z-2"/>
-      <div className="absolute bg-linear-to-l from-transparent to-black w-1/4 h-full left-0 z-2"/>
-      <div className="absolute bg-linear-to-b from-transparent to-black w-full h-1/4 bottom-0 z-2"/>
-      <Image 
-        src={background}  
-        alt={"Background image"}
-        className="w-full"
+    <div>
+      <div className="relative items-center justify-center font-sans bg-white h-auto hidden lg:block">
+        <div className="absolute bg-linear-to-r from-transparent via-black to-black w-7/8 h-full right-0 z-2"/>
+        <div className="absolute bg-linear-to-l from-transparent to-black w-1/4 h-full left-0 z-2"/>
+        <div className="absolute bg-linear-to-b from-transparent to-black w-full h-1/4 bottom-0 z-2"/>
+        <Image 
+          src={background}  
+          alt={"Background image"}
+          className="w-full"
+          loading="eager"
+          />
+        <Image 
+        src={sidebar}
+        alt="sidebar"
+        className="right-0 top-0 h-full w-auto absolute z-5"
         loading="eager"
         />
-      <Image 
-      src={sidebar}
-      alt="sidebar"
-      className="right-0 top-0 h-full w-auto absolute z-5"
-      loading="eager"
-      />
-      <div className="absolute top-[20%] right-[9%] z-6">
-        <h3 className="font-aldrich text-white text-6xl">
-          Shiina Kochiya
-        </h3>
-        <p className="font-yasashisa text-white text-xl text-center mr-12">
-        シイナ • コチヤ 
-        </p>
+        <div className="absolute top-[20%] right-[9%] z-6">
+          <h3 className="font-aldrich text-white text-6xl">
+            Shiina Kochiya
+          </h3>
+          <p className="font-yasashisa text-white text-xl text-center mr-12">
+          シイナ • コチヤ 
+          </p>
+        </div>
+        <h3 className=" absolute top-[30%] right-[9%] font-aldrich text-white text-4xl mt-4 w-5/13 z-6 text-right">
+            <span className="text-cyan-300">Code</span>, <span className="text-orange-400">Photography</span> and everything that sparks <span className="text-emerald-400">interest</span>
+        </h3> 
+
+
+        <div className="absolute top-[50%] right-[9%] z-6 w-full">
+          <Link href={"https://github.com/ShiinaKochiya/angelina-chan-sc"} className="absolute right-0">
+            <Image 
+              src={angeButton}
+              alt="ange"
+              className="h-15 w-auto cursor-pointer"
+            />
+          </Link>
+          <Link href={"https://github.com/ShiinaKochiya/balaknights"} className="absolute right-11.25 top-[53.5px]">
+            <Image 
+              src={balaButton}
+              alt="balaknights"
+              className="h-15 w-auto cursor-pointer"
+            />
+          </Link>
+          <Link href={"/photo"} className="absolute right-86.75 top-[26.75px]">
+            <Image 
+              src={photoButton}
+              alt="photo"
+              className="h-15 w-auto cursor-pointer"
+            />
+          </Link>
+        </div>
+
+        <div className="absolute top-[70%] right-[9%] z-6 flex flex-row">
+          <Link href={"https://ca-25-shiinakochiyas-projects.vercel.app"} className="h-10 w-auto cursor-pointer mx-2">
+          <Image
+            src={ca25}
+            alt="CA25"
+            className="h-10 w-auto cursor-pointer mx-2"
+          />
+          </Link>
+          <Link href={"https://github.com/ShiinaKochiya"} className="h-10 w-auto cursor-pointer mx-2">
+          <Image
+            src={github}
+            alt="GitHub"
+            className="h-10 w-auto cursor-pointer mx-2"
+          />
+          </Link>
+          <Link href={"https://www.facebook.com/ShiinaKochiya"} className="h-10 w-auto cursor-pointer mx-2">
+          <Image
+            src={facebook}
+            alt="Facebook"
+            className="h-10 w-auto cursor-pointer mx-2"
+          />
+          </Link>
+        </div>
       </div>
-      <h3 className=" absolute top-[30%] right-[9%] font-aldrich text-white text-4xl mt-4 w-5/13 z-6 text-right">
-          <span className="text-cyan-300">Code</span>, <span className="text-orange-400">Photography</span> and everything that sparks <span className="text-emerald-400">interest</span>
-      </h3> 
-
-
-      <div className="absolute top-[50%] right-[9%] z-6 w-full">
-        <Link href={"https://github.com/ShiinaKochiya/angelina-chan-sc"} className="absolute right-0">
-          <Image 
-            src={angeButton}
-            alt="ange"
-            className="h-15 w-auto cursor-pointer"
-          />
-        </Link>
-        <Link href={"https://github.com/ShiinaKochiya/balaknights"} className="absolute right-11.25 top-[53.5px]">
-          <Image 
-            src={balaButton}
-            alt="balaknights"
-            className="h-15 w-auto cursor-pointer"
-          />
-        </Link>
-        <Link href={"/photo"} className="absolute right-86.75 top-[26.75px]">
-          <Image 
-            src={photoButton}
-            alt="photo"
-            className="h-15 w-auto cursor-pointer"
-          />
-        </Link>
-      </div>
-
-      <div className="absolute top-[70%] right-[9%] z-6 flex flex-row">
-        <Link href={"https://ca-25-shiinakochiyas-projects.vercel.app"} className="h-10 w-auto cursor-pointer mx-2">
-        <Image
-          src={ca25}
-          alt="CA25"
-          className="h-10 w-auto cursor-pointer mx-2"
+      <div className="w-full h-auto from-black to-gray-800 text-white text-center lg:hidden">
+        <Image 
+          src={background}  
+          alt={"Background image"}
+          className="w-full"
+          loading="eager"
         />
-        </Link>
-        <Link href={"https://github.com/ShiinaKochiya"} className="h-10 w-auto cursor-pointer mx-2">
-        <Image
-          src={github}
-          alt="GitHub"
-          className="h-10 w-auto cursor-pointer mx-2"
-        />
-        </Link>
-        <Link href={"https://www.facebook.com/ShiinaKochiya"} className="h-10 w-auto cursor-pointer mx-2">
-        <Image
-          src={facebook}
-          alt="Facebook"
-          className="h-10 w-auto cursor-pointer mx-2"
-        />
-        </Link>
       </div>
     </div>
   );
--- app/components/stripe.tsx
+++ app/components/stripe.tsx
@@ -4,11 +4,20 @@ import React from "react";
 
 export default function Stripe({}){
      return (
-    <div className={`bg-black w-full h-20 relative overflow-hidden animate-[slide_10s_linear_infinite]`} style={
-        {
-            background: "repeating-linear-gradient(-60deg, #ffffff, #ffffff 12px, #000000 12px, #000000 45px) repeat",
-            }
-        }>
-    </div>
+        <div className="relative w-200vw h-15 overflow-hidden">
+            <div className={`bg-black absolute w-full h-20 right-[-50%] animate-[slide_10s_linear_infinite] overflow-hidden`} style={
+                {
+                    background: "repeating-linear-gradient(-60deg, #ffffff, #ffffff 12px, #000000 12px, #000000 45px) repeat",
+                    }
+                }>
+            </div>
+
+            <div className={`bg-black absolute w-full h-20 left-[-50%] animate-[slider_10s_linear_infinite] overflow-hidden`} style={
+                {
+                    background: "repeating-linear-gradient(60deg, #ffffff, #ffffff 12px, #000000 12px, #000000 45px) repeat",
+                    }
+                }>
+            </div>
+        </div>
   );
 }
